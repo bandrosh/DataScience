aa90f4574d0ea79fce9136a4914ffda0d16e7e5f
Leonardo Jales <bandrosh@gmail.com>
2020-08-15 12:07:32 -0300

    Removed old scripts

--- data.py
+++ data.py
@@ -1,14 +1,4 @@
-import NumPy.numpy_exercises as np_tests
-import Panda.panda_exercises as pd_tests
 
-print('Numpy Exercises Section: ')
-print('Test 1:')
-np_tests.test1()
-
-print('\nTest 2:')
-np_tests.test2()
-
-print('\nPanda Exercises Section: ')
 
 
 
